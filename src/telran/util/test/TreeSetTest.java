c06fa01bba1e49e1006826c6276761a9c59c78f5
Barso2 <reznitskyalex@gmail.com>
2023-07-26 23:09:38 +0300

    CW-18

---
+++ src/telran/util/test/TreeSetTest.java
@@ -0,0 +1,84 @@
+package telran.util.test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import telran.util.Collection;
+import telran.util.TreeSet;
+
+public class TreeSetTest extends SortedSetTest {
+	TreeSet<Integer> treeSet;
+
+	@BeforeEach
+	@Override
+	void setUp() {
+		collection = new TreeSet<>();
+		super.setUp();
+		treeSet = (TreeSet<Integer>) collection;
+	}
+
+	@Override
+	protected Collection<Integer> getCollection(Integer[] ar) {
+		TreeSet<Integer> res = new TreeSet<>();
+		for (Integer num : ar) {
+			res.add(num);
+		}
+		return res;
+	}
+
+	@Test
+	void displayRotatedTest() {
+		treeSet.setSpacesPerLevel(4);
+		treeSet.displayRotated();
+	}
+
+	@Test
+	void widthTest() {
+		assertEquals(3, treeSet.width());
+	}
+
+	@Test
+	void heightTest() {
+		assertEquals(4, treeSet.height());
+	}
+
+	@Test
+	void balanceTest() {
+		treeSet.balance();
+		assertEquals(4, treeSet.width());
+		assertEquals(3, treeSet.height());
+
+	}
+
+	@Test
+	void balanceTestArray() {
+		Integer[] array = new Integer[1023];
+		for (int i = 0; i < array.length; i++) {
+			array[i] = i;
+		}
+		reorderArray(array);
+		TreeSet<Integer> tree = new TreeSet<>();
+		for (Integer num : array) {
+			tree.add(num);
+		}
+		assertEquals(512, tree.width());
+
+	}
+
+	private void reorderArray(Integer[] array) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Test
+	void inverseTest() {// sorted backwards
+		Integer[] expected = { 100, 30, 14, 10, 12, 8, -20 };
+		treeSet.inverse();// TODO
+		assertArrayEquals(expected, treeSet.toArray(new Integer[0]));
+		assertTrue(treeSet.contains(100));
+	}
+}
