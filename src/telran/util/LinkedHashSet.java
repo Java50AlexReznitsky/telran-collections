520caf571d4821201949e66c9ce7e058a1617653
Barso2 <reznitskyalex@gmail.com>
2023-08-01 18:08:58 +0300

    HW-19 Yuriy solution

--- src/telran/util/LinkedHashSet.java
+++ src/telran/util/LinkedHashSet.java
@@ -2,31 +2,45 @@ package telran.util;
 
 import java.util.Iterator;
 
+import telran.util.LinkedList.Node;
+
 public class LinkedHashSet<T> implements Set<T> {
-	HashMap<T, LinkedList.Node<T>> map = new HashMap<>();
+	HashMap<T, Node<T>> map = new HashMap<>();
 	LinkedList<T> list = new LinkedList<>();
 	@Override
 	public boolean add(T obj) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean res = false;
+		if(!map.containsKey(obj)) {
+			res = true;
+			Node<T> node = new Node<>(obj);
+			map.put(obj, node);
+			list.addNode(list.size(), node);
+		}
+		return res;
 	}
 
 	@Override
 	public boolean remove(Object pattern) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean res = false;
+		
+		if(map.containsKey(pattern)) {
+			res = true;
+			Node<T> node = map.remove(pattern);
+			list.removeNode(node);
+		}
+		return res;
 	}
 
 	@Override
 	public boolean contains(Object pattern) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		return map.containsKey(pattern);
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return list.size();
 	}
 
 	@Override
@@ -37,8 +51,11 @@ public class LinkedHashSet<T> implements Set<T> {
 
 	@Override
 	public T get(Object pattern) {
-		// TODO Auto-generated method stub
-		return null;
+		T res = null;
+		if(contains(pattern)) {
+			res = map.get(pattern).obj;
+		}
+		return res;
 	}
 
 }
